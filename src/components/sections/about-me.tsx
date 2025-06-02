640c5f3e4f20131eed2d4f525b3464b6cee0cf0c
Shahzod <shahzodwill@gmail.com>
2025-06-02 16:59:56 +0200

    feat: add sections for About Me, Contact, Experiences, Hero, Skills, and Work with respective components and content

---
+++ src/components/sections/about-me.tsx
@@ -0,0 +1,92 @@
+import Image from "next/image";
+
+import ShakhzodFull from "/public/images/shakhzod-full.jpg";
+import Tag from "@/components/data-display/tag";
+import Container from "@/components/layout/container";
+import Typography from "@/components/general/typography";
+import Link from "@/components/navigation/link";
+import { EXTERNAL_LINKS } from "@/lib/data";
+
+const AboutMeSection = () => {
+  return (
+    <Container className="bg-gray-50" id="about">
+      <div className="self-center">
+        <Tag label="About me" />
+      </div>
+
+      <div className="flex w-full flex-col justify-between gap-12 md:flex-row">
+        {/* Image */}
+        <div className="flex justify-center md:order-first md:justify-end">
+          <div className="relative h-[380px] w-[320px] md:h-[460px] md:w-[380px] lg:h-[520px] lg:w-[440px]">
+            <Image
+              src={ShakhzodFull}
+              alt="Shakhzod on Dragon"
+              className="absolute z-10 h-[360px] w-[280px] border-8 border-gray-50 max-md:left-5 md:right-0 md:top-0 md:h-[420px] md:w-[340px] lg:h-[480px] lg:w-[400px]"
+              style={{ objectFit: "cover" }}
+            ></Image>
+            <div className="absolute h-[360px] w-[320px] border-8 border-transparent bg-gray-200 max-md:top-5 md:bottom-0 md:left-0 md:h-[420px] md:w-[340px] lg:h-[480px] lg:w-[400px]"></div>
+          </div>
+        </div>
+
+        {/* Content */}
+        <div className="flex max-w-xl flex-col gap-6">
+          <Typography>
+            I&apos;m a frontend developer with a strong foundation in modern web
+            development, currently I&apos;m finishing studies at Noroff. I began
+            my journey in August 2023, and since then, I&apos;ve built up skills
+            in HTML, CSS, JavaScript, and Tailwind CSS for styling. I&apos;ve
+            also worked with Figma for UI design and used Vite for efficient
+            tooling in my projects. And have been using Git actively for all the
+            assignments for Noroff ({" "}
+            <Link
+              noCustomization
+              externalLink
+              withUnderline
+              href={EXTERNAL_LINKS.GITHUB}
+            >
+              GitHub
+            </Link>
+            ). Lately, I&apos;ve been diving deeper into React and TypeScript to
+            build more scalable and dynamic applications. I&apos;m passionate
+            about clean, accessible code and continuously learning to stay up to
+            date with the latest in frontend technologies.
+          </Typography>
+          <Typography>
+            I am very much a progressive thinker and enjoy working on products
+            end to end, from ideation all the way to development.
+          </Typography>
+          <Typography>
+            I&apos;m interested in investing and keen on managing finances
+            effectively, so in my spare time I'd like to stay updated in
+            investment oppportunities and financing. Also, I enjoy gaming from
+            time to time.
+          </Typography>
+          <Typography>Some quick bits about me:</Typography>
+          <div className="flex flex-col gap-2 md:flex-row md:gap-6">
+            <ul className="flex list-inside list-disc flex-col gap-2">
+              <Typography component="li">
+                B.A. in Financial management
+              </Typography>
+              <Typography component="li">
+                Certificate in HTML/CSS mark-up course
+              </Typography>
+              <Typography component="li">
+                Noroff frontend development graduate
+              </Typography>
+            </ul>
+            <ul className="flex list-inside list-disc flex-col gap-2">
+              <Typography component="li">Avid learner</Typography>
+            </ul>
+          </div>
+          <Typography>
+            One last thing, I&apos;m available for freelance works, and
+            internship positions to build more experiences working on bigger
+            projects, so feel free to reach out!
+          </Typography>
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default AboutMeSection;
---
+++ src/components/sections/hero.tsx
@@ -0,0 +1,69 @@
+import Image from "next/image";
+import { MapPin } from "lucide-react";
+
+import ShakhzodFront from "/public/images/shakhzod-front.jpg";
+import SocialIcons from "@/components/data-display/social-icons";
+import Typography from "@/components/general/typography";
+import Container from "@/components/layout/container";
+
+const HeroSection = () => {
+  return (
+    <Container id="hero">
+      <div className="flex flex-col gap-12 md:flex-row">
+        {/* Image */}
+        <div className="flex items-center justify-center md:order-last md:flex-grow md:justify-end">
+          <div className="relative h-[300px] w-[280px] md:h-[360px] md:w-[320px]">
+            <Image
+              src={ShakhzodFront}
+              alt="Headshot of Shakhzod"
+              className="absolute z-10 h-[280px] w-[240px] border-8 border-gray max-md:left-5 md:left-0 md:top-0 md:h-[320px] md:w-[280px]"
+              style={{ objectFit: "cover" }}
+            ></Image>
+            <div className="absolute h-[280px] w-[280px] border-8 border-transparent bg-gray-200 max-md:top-5 md:bottom-0 md:right-0 md:h-[320px] md:w-[280px]"></div>
+          </div>
+        </div>
+
+        {/* Content */}
+        <div className="flex max-w-3xl flex-grow flex-col justify-center gap-8 md:order-first md:items-start md:justify-center 2xl:gap-12">
+          <div className="flex flex-col gap-2">
+            <Typography variant="h1">
+              Hi, I&apos;m Shakhzod{" "}
+              <span className="inline-block animate-waving-hand">👋</span>
+            </Typography>
+            <Typography>
+              A dedicated front-end developer with a passion for creating
+              stunning and responsive web applications. I've been honing my
+              skills in ReactJS, and JavaScript for the past year at Noroff
+              School of Technology and Digital Media. Currently, I'm diving into
+              the world of Typescript to build even more dynamic user
+              interfaces. When I'm not coding, I enjoy exploring new
+              technologies, reading tech blogs, and collaborating with other
+              developers on exciting projects. I&apos;m always eager to take on
+              new challenges and contribute to projects that push the boundaries
+              of web development.
+            </Typography>
+          </div>
+          <div className="flex flex-col gap-2">
+            <div className="flex gap-2">
+              <MapPin className="stroke-gray-600" />
+              <Typography>Kristiansand, Norway</Typography>
+            </div>
+
+            <div className="flex items-center gap-2">
+              <div className="flex h-6 w-6 items-center justify-center">
+                <span className="relative flex h-3 w-3">
+                  <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-emerald-400 opacity-75"></span>
+                  <span className="relative inline-flex h-3 w-3 rounded-full bg-emerald-500"></span>
+                </span>
+              </div>
+              <Typography>Available for new projects</Typography>
+            </div>
+          </div>
+          <SocialIcons />
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default HeroSection;
